af18835da93c8c0ff84ec6dc5ebfe1ac64a71444
Guillaume Crégut <gcregut@free.fr>
2023-09-28 18:28:19 +0200

    cleaning index php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-use Editiel98\SmartyMKD;
 use Editiel98\Autoloader;
 use Editiel98\App;
 require '../src/classes/Autoloader.php';
@@ -8,12 +7,5 @@ session_start();
 $app=new App();
 $app->run();
 
-/*
-$smarty = new SmartyMKD();
-
-
-$smarty->assign('accueil','accueil');
-
-$smarty->display('index.tpl');*/
 
 ?>

b8cfbb5861adf3c0521289a35e7fecca37fb82e6
Guillaume Crégut <gcregut@free.fr>
2023-09-28 13:24:38 +0200

    change to add router

--- public/index.php
+++ public/index.php
@@ -1,15 +1,19 @@
 <?php
 use Editiel98\SmartyMKD;
 use Editiel98\Autoloader;
-
+use Editiel98\App;
 require '../src/classes/Autoloader.php';
 Autoloader::register();
 session_start();
+$app=new App();
+$app->run();
+
+/*
 $smarty = new SmartyMKD();
 
 
 $smarty->assign('accueil','accueil');
 
-$smarty->display('index.tpl');
+$smarty->display('index.tpl');*/
 
 ?>

3646f3cb3906135d798d16e692005017a6fc3f22
Guillaume Crégut <gcregut@free.fr>
2023-09-28 12:21:59 +0200

    move index.php

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+use Editiel98\SmartyMKD;
+use Editiel98\Autoloader;
+
+require '../src/classes/Autoloader.php';
+Autoloader::register();
+session_start();
+$smarty = new SmartyMKD();
+
+
+$smarty->assign('accueil','accueil');
+
+$smarty->display('index.tpl');
+
+?>

3dfbd541036c960c7d2ddfd0ed43a8809d4851c9
Guillaume Crégut <gcregut@free.fr>
2023-09-29 11:43:09 +0200

    update country manager to new structure

--- src/classes/Manager/CountryManager.php
+++ src/classes/Manager/CountryManager.php
@@ -2,42 +2,13 @@
 namespace Editiel98\Manager;
 
 use Editiel98\Database\Database;
-use Editiel98\Entity\Country;
 
-class CountryManager
+class CountryManager extends SingleManager
 {
-    private Database $db;
     public function __construct(Database $db)
     {
         $this->db=$db;
-    }
-
-    public function __get($name)
-    {
-        $method='get' . ucfirst($name);
-        return $this->$method();
-    }
-
-    /**
-     * 
-     * Return all countries
-     * @return array
-     */
-    public function getAll():array
-    {
-        $result=$this->db->query('SELECT * FROM country','Editiel98\Entity\Country');
-        return $result;
-    }
-/**
- * 
- * get 
- * @param integer $id
- * @return Country
- */
-    public function findById(int $id): Country
-    {
-        $query='SELECT * FROM country WHERE id=:id';
-        $result=$this->db->prepare($query,'Editiel98\Entity\Country',[':id'=>$id],true);
-        return $result;
+        $this->table='country';
+        $this->className='Editiel98\Entity\Country';
     }
 }

f2e79ba66f2d5c715aee1887456916307846e179
Guillaume Crégut <gcregut@free.fr>
2023-09-29 20:56:01 +0200

    [feat]: SingleManager: add CRUD functions

--- src/classes/Manager/SingleManager.php
+++ src/classes/Manager/SingleManager.php
@@ -1,10 +1,12 @@
 <?php
 namespace Editiel98\Manager;
 
+use Editiel98\Entity\Entity;
+
 /**
  * Manage single entity to DB
  */
-abstract class SingleManager extends Manager
+abstract class SingleManager extends Manager implements ManagerInterface
 {
     /**
      * Get all datas from DB for the entity
@@ -13,7 +15,7 @@ abstract class SingleManager extends Manager
      */
     public function getAll():array
     {
-        $result=$this->db->query('SELECT id,name FROM ' . $this->table,$this->className); //'Editiel98\Entity\Country'
+        $result=$this->db->query('SELECT id,name FROM ' . $this->table,$this->className); 
         return $result;
     }
 
@@ -23,7 +25,7 @@ abstract class SingleManager extends Manager
      * @param integer $id
      * @return object : class of the entity
      */
-    public function findById(int $id):object
+    public function findById(int $id): Entity
     {
         $query='SELECT id, name FROM ' . $this->table.' WHERE id=:id';
         $result=$this->db->prepare($query,$this->className,[':id'=>$id],true); 
@@ -36,10 +38,31 @@ abstract class SingleManager extends Manager
      * @param string $name
      * @return object class of the entity
      */
-    public function findByName(string $name):object
+    public function findByName(string $name):Entity
     {
-        $query='SELECT id, name FROM ' . $this->table.' WHERE name=:name';
+        $query='SELECT id, name FROM ' . $this->table .' WHERE name=:name';
         $result=$this->db->prepare($query,$this->className,[':name'=>$name],true); 
+        return $result;
+    }
+
+    public function update(Entity $entity): bool{
+        $query='UPDATE ' . $this->table .' SET name=:name WHERE id=:id';
+        $name=$entity->getName();
+        $id=$entity->getId();
+        return $this->db->exec($query,['name'=>$name,':id'=>$id]);
+    }
+
+    public function save(Entity $entity): bool{
+        $query='INSERT INTO ' . $this->table . '(name) VALUES (:name)';
+        $name=$entity->getName();
+        $result=$this->db->exec($query,['name'=>$name]);
+        return $result;
+    }
+
+    public function delete(Entity $entity) : bool{
+        $query='DELETE FROM ' . $this->table . ' WHERE id=:id';
+        $id=$entity->getId();
+        $result=$this->db->exec($query,['id'=>$id]);
         return $result;
     }
 }

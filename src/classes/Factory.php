4beb57f114f0c3841d39c6bf2d834f68ceb88fea
Guillaume Crégut <gcregut@free.fr>
2023-09-29 20:51:36 +0200

    [feat]: Factory: Add getManager function

--- src/classes/Factory.php
+++ src/classes/Factory.php
@@ -1,12 +1,14 @@
 <?php
 namespace Editiel98;
 
+use Editiel98\Database\Database;
 use Editiel98\Entity\Entity;
+use Editiel98\Manager\Manager;
 
 class Factory
 {
     private static $_instance;
-
+    private static $db;
 
     public static function getInstance()
     {
@@ -20,6 +22,22 @@ class Factory
     {
         $className='Editiel98\\Entity\\' . ucFirst($name);
         return new $className;
+    }
+
+    public static function getdB(): Database{
+        if(is_null(self::$db)){
+            self::$db=new Database();
+        }
+        return self::$db;
+    }
+
+    public static function getManager(string $name): Manager
+    {
+        if(is_null(self::$db)){
+            self::getdB();
+        }
+        $className='Editiel98\\Manager\\' . ucFirst($name);
+        return new $className(self::$db);
     }
 
 }

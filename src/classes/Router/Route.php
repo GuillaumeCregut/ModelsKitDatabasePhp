d0c0289ec9eb5609e9cd562c006f995973fe0bd4
Guillaume Crégut <gcregut@free.fr>
2023-09-30 00:57:57 +0200

    [feat]Route: add suroutes and params

--- src/classes/Router/Route.php
+++ src/classes/Router/Route.php
@@ -6,10 +6,14 @@ use Editiel98\SmartyMKD;
 abstract class Route
 {
     protected SmartyMKD $smarty;
+    protected array $subPages;
+    protected array $params;
 
-    public function __construct()
+    public function __construct(array $subPages=null, array $params=null)
     {
         $this->smarty=new SmartyMKD();
+        $this->subPages=$subPages;
+        $this->params=$params;
     }
     abstract public function render();
 

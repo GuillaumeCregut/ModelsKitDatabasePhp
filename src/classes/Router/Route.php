eda5f2882ecbcbed627dab6773ec8c5c9d9290d7
Guillaume Crégut <gcregut@free.fr>
2023-09-30 18:15:59 +0200

    [feat]: Route: add credentials management

--- src/classes/Router/Route.php
+++ src/classes/Router/Route.php
@@ -1,6 +1,7 @@
 <?php
 namespace Editiel98\Router;
 
+use Editiel98\App;
 use Editiel98\SmartyMKD;
 
 abstract class Route
@@ -16,6 +17,16 @@ abstract class Route
         $this->params=$params;
     }
     abstract public function render();
-
+    protected function getCredentials(){
+        if(isset($_SESSION['isConnected'])){
+            if($_SESSION['isConnected']){
+                $this->smarty->assign('logged_in','accueil');
+                $this->smarty->assign('fullname',$_SESSION['fullName']);
+                if (App::ADMIN===$_SESSION['rankUser']){
+                    $this->smarty->assign('loggedInAdmin','true');
+                }
+            }
+        }
+    }
 
 }

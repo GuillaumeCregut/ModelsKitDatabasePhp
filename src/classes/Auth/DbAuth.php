da6d9003027a364a3f0806bd2abdc1e0c29e707f
Guillaume Crégut <gcregut@free.fr>
2023-09-30 23:25:55 +0200

    [feat]: DbAuth: create function to register user

--- src/classes/Auth/DbAuth.php
+++ src/classes/Auth/DbAuth.php
@@ -1,10 +1,12 @@
 <?php
 namespace Editiel98\Auth;
 
+use App\Controller\Error;
 use Editiel98\Database\Database;
 use Editiel98\DbException;
 use Editiel98\Entity\User;
 use Editiel98\Factory;
+
 use Exception;
 
 
@@ -20,10 +22,10 @@ class DbAuth
 
     public function login(string $login, string $password):bool
     {
-        $query='SELECT firstname,id,lastname,rankUser,passwd,avatar FROM user WHERE login=:login';
+        $query='SELECT firstname,id,lastname,rankUser,passwd,avatar,isvalid FROM user WHERE login=:login';
         try{
             $userDb=$this->db->prepare($query,null,[':login'=>$login],true);
-            $isValid=password_verify( $password,  $userDb->passwd);
+            $isValid=password_verify( $password,  $userDb->passwd)&&($userDb->isvalid===1);
             if($isValid){
                 $user=new User(
                     $userDb->firstname,
@@ -44,8 +46,36 @@ class DbAuth
         }
     }
 
-    public function isLogged() : bool
+    // public function isLogged() : bool
+    // {
+    //     return isset($_SESSION['auth']);
+    // }
+
+    public function signUp(string $login, string $mail, string $firstname, string $lastname, string $pass): bool
     {
-        return isset($_SESSION['auth']);
+        $hashedPassword=password_hash($pass,PASSWORD_DEFAULT);
+        $query="INSERT INTO user 
+            (firstname, lastname, login,passwd,rankUser, email, isvalid) 
+            VALUES(:firstname,:lastname,:login,:pass,1,:email,0)";
+        $values=[
+            ':firstname'=>$firstname,
+            ':lastname'=>$lastname,
+            ':email'=>$mail,
+            ':login'=>$login,
+            'pass'=>$hashedPassword
+        ];
+        try{
+            $result=$this->db->exec($query,$values);
+            return $result;
+        }
+        catch(DbException $e){
+            switch($e->getDbCode()){
+                case 23000: $message="L'uilisateur existe déjà";
+                    break;
+                default: $message=$e->getdbMessage();
+            }
+            throw new Exception($message);
+        }
+        return false;
     }
 }

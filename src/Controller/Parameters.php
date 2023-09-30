710a5ab9303c231bc61bf0ddbe0bc42b189d0cb6
Guillaume Crégut <gcregut@free.fr>
2023-09-30 11:49:49 +0200

    [feat]: Parameters: create sub router

--- src/Controller/Parameters.php
+++ src/Controller/Parameters.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Editiel98\Router\Route;
@@ -8,9 +9,40 @@ class Parameters extends Route
     public function render()
     {
         var_dump($this->subPages);
-        echo "param";
-      /*  $this->smarty->assign('params','params');
-        $this->smarty->display('params/index.tpl');*/
-        
+        if (empty($this->subPages)) {
+            $this->smarty->assign('params','params');
+            $this->smarty->display('params/index.tpl');
+        } else {
+            switch ($this->subPages[0]) {
+                case 'country':
+                    $className = 'Country';
+                    break;
+                case 'brand':
+                    $className = 'Brand';
+                    break;
+                case 'period':
+                    $className = 'Period';
+                    break;
+                case 'builder':
+                    $className = 'Builder';
+                    break;
+                case 'category':
+                    $className = 'Category';
+                    break;
+                case 'scale':
+                    $className = 'Scale';
+                    break;
+                case 'model':
+                    $className = 'Model';
+                    break;
+                default: 
+                    $page=new Error('404');
+                    $page->render();
+                    die();
+            }
+            $classPage = 'App\\Controller\\Parameters\\' . $className;
+            $page=new $classPage($this->params);
+            $page->render();
+        }
     }
 }

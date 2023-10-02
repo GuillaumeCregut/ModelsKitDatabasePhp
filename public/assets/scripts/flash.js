d3804311de92336df6921b4af862321ea5429bfa
Guillaume Crégut <gcregut@free.fr>
2023-10-02 13:21:40 +0200

    [refactor]: flash JS: remove console log

--- public/assets/scripts/flash.js
+++ public/assets/scripts/flash.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM fully loaded and parsed");
     const flashes=document.querySelectorAll('li.toast');
-    console.log(flashes);
     flashes.forEach((flashItem)=>{
         flashItem.timeOutId=setTimeout(()=>removeToast(flashItem),5000)
     })
@@ -9,7 +7,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 const removeToast=(toast)=>{
     toast.classList.add('toast_hide');
-    console.log("hello");
     if(toast.timeOutId)
         clearTimeout(toast.timeOutId);
     setTimeout(()=>toast.remove(),500);

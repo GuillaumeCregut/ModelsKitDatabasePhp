a7ddac01afa92c2ab9195633f99e151beef05366
Guillaume Crégut <gcregut@free.fr>
2023-10-03 16:17:30 +0200

    [feat]: Logger: create logger and logger classes

---
+++ src/classes/Logger/InfoLogger.php
@@ -0,0 +1,9 @@
+<?php
+namespace Editiel98\Logger;
+
+class InfoLogger extends Logger
+{
+    public function __construct(){
+        parent::__construct('info');
+    }
+}
---
+++ src/classes/Logger/Logger.php
@@ -0,0 +1,63 @@
+<?php
+namespace Editiel98\Logger;
+
+use App\Controller\Error;
+use DateTime;
+use Exception;
+
+abstract class Logger implements LoggerInterface
+{
+    private string $filename;
+   
+    public function __construct(string $filename)
+    {
+        $directory=dirname(dirname(__DIR__)) . '/logs/';
+        $this->filename=$directory . $filename . '.log';
+    }
+
+    public function storeToFile( string $value): bool
+    {
+        date_default_timezone_set('Europe/Paris');
+        $now=new DateTime();
+        $dateMessage=date_format($now, 'd/m/Y H:i:s');
+        $message=$dateMessage . ' : ' .$value ."\n"; 
+        try{
+            if (is_writable($this->filename)) {
+                if($file=fopen($this->filename,'a')){
+                    fwrite($file,$message);
+                    fclose($file);
+                    return true;
+                }
+                else{
+                    fclose($file);
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        catch(Exception $e){
+            $error=new Error($e->getMessage());
+            $error->render();
+            die();
+        }
+        
+    }
+
+    public function loadFromFile(): array|bool
+    {
+        $logs=[];
+        if($file=fopen($this->filename,'r')){
+            while(!feof($file)){
+                $line=fgets($file);
+                if($line){
+                    $logs[]=$line;
+                }
+            }
+            return $logs;
+        }else{
+            return false;
+        }
+    }
+}

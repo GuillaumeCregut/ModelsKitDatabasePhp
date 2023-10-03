415389c80ce25e779ef3c977a6ffddff2e4c378d
Guillaume Crégut <gcregut@free.fr>
2023-10-03 13:09:39 +0200

    [feat]: SingleManager: manage DB execptions

--- src/classes/Manager/SingleManager.php
+++ src/classes/Manager/SingleManager.php
@@ -1,7 +1,10 @@
 <?php
 namespace Editiel98\Manager;
 
+use App\Controller\Error as ControllerError;
+use Editiel98\DbException;
 use Editiel98\Entity\Entity;
+use Error;
 
 /**
  * Manage single entity to DB
@@ -15,8 +18,13 @@ abstract class SingleManager extends Manager implements ManagerInterface
      */
     public function getAll():array
     {
-        $result=$this->db->query('SELECT id,name FROM ' . $this->table,$this->className); 
-        return $result;
+        try{
+            $result=$this->db->query('SELECT id,name FROM ' . $this->table,$this->className); 
+            return $result;
+        }
+        catch(DbException $e){
+            $this->loadErrorPage($e->getdbMessage());
+        }
     }
 
     /**
@@ -28,7 +36,7 @@ abstract class SingleManager extends Manager implements ManagerInterface
     public function findById(int $id): Entity
     {
         $query='SELECT id, name FROM ' . $this->table.' WHERE id=:id';
-        $result=$this->db->prepare($query,$this->className,[':id'=>$id],true); 
+        $result=$this->prepareSQL($query,[':id'=>$id],true); 
         return $result;
     }
 
@@ -41,28 +49,99 @@ abstract class SingleManager extends Manager implements ManagerInterface
     public function findByName(string $name):Entity
     {
         $query='SELECT id, name FROM ' . $this->table .' WHERE name=:name';
-        $result=$this->db->prepare($query,$this->className,[':name'=>$name],true); 
+        $result=$this->prepareSQL($query,[':name'=>$name],true); 
         return $result;
     }
 
+    /**
+     * Update an entity in DB
+     *
+     * @param Entity $entity : entity to update
+     * @return boolean
+     */
     public function update(Entity $entity): bool{
         $query='UPDATE ' . $this->table .' SET name=:name WHERE id=:id';
         $name=$entity->getName();
         $id=$entity->getId();
-        return $this->db->exec($query,['name'=>$name,':id'=>$id]);
+        return $this->execSQL($query,['name'=>$name,':id'=>$id]);
     }
 
+    /**
+     * Store Entity in Database
+     *
+     * @param Entity $entity : Entity to store in DB
+     * @return boolean
+     */
     public function save(Entity $entity): bool{
         $query='INSERT INTO ' . $this->table . '(name) VALUES (:name)';
-        $name=$entity->getName();
-        $result=$this->db->exec($query,['name'=>$name]);
+        $name=$entity->getName();  
+        $result=$this->execSQL($query,['name'=>$name]);
         return $result;
     }
 
+    /**
+     * Delete an entity into DB
+     *
+     * @param Entity $entity : Entity to delete
+     * @return boolean
+     */
     public function delete(Entity $entity) : bool{
         $query='DELETE FROM ' . $this->table . ' WHERE id=:id';
         $id=$entity->getId();
-        $result=$this->db->exec($query,['id'=>$id]);
+        $result=$this->execSQL($query,['id'=>$id]);
         return $result;
+    }
+
+    /**
+     * Exec the query
+     *
+     * @param string $query : Query to execute
+     * @param array $vars : vars for the query
+     * @return mixed
+     */
+    private function execSQL(string $query,array $vars): mixed
+    {
+        try{
+            $result=$this->db->exec($query,$vars);
+            return $result;
+        }
+        catch(DbException $e){
+            $this->loadErrorPage($e->getdbMessage());
+        }
+
+    }
+
+    /**
+     * Execute a prepared query
+     *
+     * @param string $query : query to execute
+     * @param array $vars : vars for the query
+     * @param boolean $single : return one result or not
+     * @return mixed
+     */
+    private function prepareSQL(string $query, array $vars, bool $single): mixed
+    {
+        try{
+            $result=$this->db->prepare($query,$this->className,$vars,$single);
+            return $result;
+        }
+        catch(DbException $e){
+            $this->loadErrorPage($e->getMessage());
+        }
+    }
+
+    /**
+     * On DB Error, log to lofile, and display error page
+     *
+     * @param string $message : Error message from DB
+     * @return void
+     */
+    private function loadErrorPage(string $message)
+    {
+        //Log to error log
+        //display error page
+        $errPage=new ControllerError('500',$message);
+        $errPage->render();
+        die();
     }
 }

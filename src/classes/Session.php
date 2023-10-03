3a93867f656a8769c05aed6a60bcabb443ab4211
Guillaume Crégut <gcregut@free.fr>
2023-10-03 12:22:23 +0200

    [feat]: Session : add const and destroy function, docs

--- src/classes/Session.php
+++ src/classes/Session.php
@@ -5,12 +5,24 @@ use Editiel98\Interfaces\SessionInterface;
 
 class Session implements SessionInterface
 {
+    const SESSION_CONNECTED='isConnected';
+    const SESSION_FULLNAME='fullName';
+    const SESSION_RANK_USER='rankUser';
+    const SESSION_USER_ID='userId';
+    
+    
     public function __construct()
     {
         if(session_status()===PHP_SESSION_NONE)
         session_start();
     }
     
+    /**
+     * Get a Key in Session
+     *
+     * @param string $key : key in session
+     * @return mixed value of the key
+     */
     public function getKey(string $key): mixed
     {
         if(isset( $_SESSION[$key])){
@@ -20,17 +32,47 @@ class Session implements SessionInterface
         }
     }
 
+    /**
+     * Store a key in session
+     *
+     * @param string $key : name of the key
+     * @param mixed $value : value to store
+     * @return void
+     */
     public function setKey(string $key, mixed $value)
     {
         $_SESSION[$key]=$value;
     }
 
+    /**
+     * Store an array for the key in session
+     *
+     * @param string $key : key to store
+     * @param mixed $value : values to store for the key
+     * @return void
+     */
     public function setMultipleKey(string $key, mixed $value){
         $_SESSION[$key][]=$value;
     }
 
+    /**
+     * delete a key from session
+     *
+     * @param string $key : key to remove
+     * @return void
+     */
     public function deleteKey(string $key)
     {
         unset($_SESSION[$key]);
+    }
+
+    /**
+     * Destroy the session
+     *
+     * @return void
+     */
+    public function destroy()
+    {
+        session_destroy();
     }
 }

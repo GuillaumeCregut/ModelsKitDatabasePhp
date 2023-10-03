ee2fb3dcb5529ebdb3eb76ec3825e5340584b427
Guillaume Crégut <gcregut@free.fr>
2023-10-03 19:03:21 +0200

    [feat]: AdminLogs: change security check

--- src/Controller/Admin/Logs.php
+++ src/Controller/Admin/Logs.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller\Admin;
 
+use Editiel98\App;
 use Editiel98\Logger\ErrorLogger;
 use Editiel98\Logger\InfoLogger;
 use Editiel98\Logger\WarnLogger;
@@ -10,17 +11,23 @@ class Logs extends Controller
 {
     public function render()
     {
-        $this->smarty->assign('adminLogs_menu','true');
-        //Get all Logs
-        $infoLog=new InfoLogger();
-        $infoLogs=$infoLog->loadFromFile();
-        $warnLog=new WarnLogger();
-        $warnLogs=@$warnLog->loadFromFile();
-        $errLog=new ErrorLogger();
-        $errorLogs=$errLog->loadFromFile();
-        $this->smarty->assign('warnLogs',$warnLogs);
-        $this->smarty->assign('errorLogs',$errorLogs);
-        $this->smarty->assign('infoLogs',$infoLogs);
-        $this->smarty->display('admin/logs.tpl');
+        if(App::ADMIN===$this->userRank){
+            $this->smarty->assign('adminLogs_menu','true');
+            //Get all Logs
+            $infoLog=new InfoLogger();
+            $infoLogs=$infoLog->loadFromFile();
+            $warnLog=new WarnLogger();
+            $warnLogs=@$warnLog->loadFromFile();
+            $errLog=new ErrorLogger();
+            $errorLogs=$errLog->loadFromFile();
+            $this->smarty->assign('warnLogs',$warnLogs);
+            $this->smarty->assign('errorLogs',$errorLogs);
+            $this->smarty->assign('infoLogs',$infoLogs);
+            $this->smarty->display('admin/logs.tpl');
+        } else{
+            $this->smarty->assign('accueil','accueil');
+            $this->smarty->display('index.tpl');
+        }
+
     }
 }

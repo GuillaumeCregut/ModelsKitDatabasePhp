060eb088e165e6870ed3064f9e543ea59452dac5
Guillaume Crégut <gcregut@free.fr>
2023-10-04 19:28:10 +0200

    [Feat]: changeRankUser: finishing method

--- src/classes/Api/Admin/ChangeRankUser.php
+++ src/classes/Api/Admin/ChangeRankUser.php
@@ -2,22 +2,29 @@
 namespace Editiel98\Api\Admin;
 
 use Editiel98\App;
+use Editiel98\Event\Emitter;
+use Editiel98\Manager\UserManager;
 use Editiel98\Router\ApiController;
+use Exception;
 
 class ChangeRankUser extends ApiController
 {
     public function manage()
     {
+        error_reporting(0);
         if($this->isConnected){
             if(App::ADMIN==$this->userRank){
                 $method=$_SERVER['REQUEST_METHOD'];
                 switch ($method){
-                    case 'GET': $this->updateUser();
-                        
+                    case 'GET': 
+                        header("HTTP/1.1 405 Method Not Allowed");
+                        die();
                         break;
-                    case 'POST': $this->updateUser();
+                    case 'POST': 
+                        header("HTTP/1.1 405 Method Not Allowed");
+                        die();
                         break;
-                    case 'PUT': header("HTTP/1.1 405 Method Not Allowed");
+                    case 'PUT': $this->updateUser();
                         break;
                     case 'DELETE':
                         header("HTTP/1.1 405 Method Not Allowed");
@@ -29,11 +36,11 @@ class ChangeRankUser extends ApiController
                 }
             }
             else{
-             //   header("HTTP/1.1 403 Forbidden");
+                header("HTTP/1.1 403 Forbidden");
             }
         }
         else{
-          //  header("HTTP/1.1 401 Unauthorized");
+            header("HTTP/1.1 401 Unauthorized");
         }
     }
 
@@ -45,7 +52,43 @@ class ChangeRankUser extends ApiController
             'Utilisateur'=>$datas->idUser,
             'NewValue'=>$datas->newStatus
         ];
-        echo (json_encode($arrayData));
-    
+        $idUser=$datas->idUser;
+        $newStatus=$datas->newStatus;
+        if(!is_int($idUser) ||!is_bool($newStatus)){
+            $return=[
+                "result"=>false,
+                'Utilisateur'=>$idUser,
+                'NewValue'=>$newStatus,
+            ];
+            header("HTTP/1.1 422 Unprocessable entity");
+            echo json_encode($return);
+            die();
+        }
+        $userManager=new UserManager($this->dbConnection);
+        try{
+            $user=$userManager->findById($idUser);
+        }
+        catch(Exception $e){
+            header("HTTP/1.1 500 Unprocessable entity");
+            die();
+        }
+        try{
+            $result=$userManager->setNewStatus($idUser,$newStatus);
+            $result=!!$result;
+            if($newStatus && $result){
+                $emitter=Emitter::getInstance();
+                $emitter->emit(Emitter::USER_VALIDATED,$user->getEmail());
+            }
+            $arrayData=[
+                'Utilisateur'=>$idUser,
+                'NewValue'=>$newStatus,
+                'result'=>$result
+            ];
+            echo (json_encode($arrayData));
+        }
+        catch(Exception $e){
+            header("HTTP/1.1 500 Unprocessable entity");
+            die();
+        }
     }
 }

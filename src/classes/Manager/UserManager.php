311ff2fdf7cc575d12b9ad8bbab2bd37f3bd70ef
Guillaume Crégut <gcregut@free.fr>
2023-10-04 19:25:55 +0200

    [Feat]: UserManager: add find methods

--- src/classes/Manager/UserManager.php
+++ src/classes/Manager/UserManager.php
@@ -3,6 +3,8 @@ namespace Editiel98\Manager;
 
 use Editiel98\Database\Database;
 use Editiel98\DbException;
+use Editiel98\Entity\User;
+use Exception;
 
 class UserManager extends Manager //implements ManagerInterface
 {
@@ -13,9 +15,18 @@ class UserManager extends Manager //implements ManagerInterface
         $this->className='Editiel98\Entity\User';
     }
 
-    // public function findById(int $id): Entity{
-        
-    // }
+     public function findById(int $id): User|bool
+     {
+        try{
+            $query="SELECT firstname, lastname,email, rankUser, id, email,isvalid FROM " . $this->table . " WHERE id=:id";
+            $classname='Editiel98\Entity\User';
+            $values=[':id'=>$id];
+            return $this->db->prepare($query,$classname,$values,true);
+        }
+        catch(DbException $e){
+           throw new Exception($e->getdbMessage());
+        }
+     }
 
     public function getAll(): array
     {
@@ -27,24 +38,45 @@ class UserManager extends Manager //implements ManagerInterface
             $this->loadErrorPage($e->getdbMessage());
         }
     }
-/*
-    public function findByName(string $name): Entity
-    {
 
+    public function findByName(string $lastname): User|bool
+    {
+        try{
+            $query="SELECT firstname, lastname,email, rankUser, id, email,isvalid FROM " . $this->table . " WHERE login=:login";
+            $classname='Editiel98\Entity\User';
+            $values=[':id'=>$lastname];
+            return $this->db->prepare($query,$classname,$values,true);
+        }
+        catch(DbException $e){
+           throw new Exception($e->getdbMessage());
+        }
     }
 
-    public function update(Entity $entity): bool
-    {
-
-    }
-
-    public function save(Entity $entity): bool
-    {
-
-    }
-
-    public function delete(Entity $entity) : bool
+    public function update(USer $entity): bool
     {
         return false;
-    }*/
+    }
+
+    public function save(USer $entity): bool
+    {
+        return false;
+    }
+
+    public function delete(USer $entity) : bool
+    {
+        return false;
+    }
+
+    public function setNewStatus(int $user, bool $status) :bool
+    {
+        $query="UPDATE " . $this->table . " SET isvalid=:valid WHERE id=:id";
+        $values=[":valid"=>$status,":id"=>$user];
+        try{
+            $test=$this->db->exec($query,$values);
+            return $test;
+        }
+        catch(DbException $e){
+           throw new Exception($e->getdbMessage());
+        }
+    }
 }

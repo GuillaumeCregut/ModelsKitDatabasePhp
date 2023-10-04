7c0337db0ec78a722b7e18a39883f0fec540bfe1
Guillaume Crégut <gcregut@free.fr>
2023-10-04 19:16:56 +0200

    [Feat]: admin js: changing user state OK

--- public/assets/scripts/admin_user.js
+++ public/assets/scripts/admin_user.js
@@ -1,7 +1,22 @@
+const toastDetails={
+    timer: 5000,
+    success:{
+        icon: "fa-circle-check",
+        classname: "toast_success"
+    },
+    error:{
+        icon: "fa-circle-xmark",
+        classname:"toast_error"
+    }
+}
+
+
+
+
 const checkboxes=document.querySelectorAll('input.cb_user_valid');
 checkboxes.forEach((cb)=>{
     cb.addEventListener('change',()=>{
-        changeUserStatus(cb.dataset.id,cb.checked);
+        changeUserStatus(cb.dataset.id,cb.checked,cb);
     })
 })
 
@@ -12,23 +27,70 @@ listRole.forEach((list)=>{
     })
 })
 
-
-
-const changeUserStatus=(id,status)=>{
+const changeUserStatus=(id,status,caller)=>{
+    const oldStatus=!status;
+    let sentFlash=false;
+    const idUser=parseInt(id);
     const myInit = {
-        method: "POST",
+        method: "PUT",
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
-        body:JSON.stringify({idUser:id, newStatus:status})
+        body:JSON.stringify({idUser:idUser, newStatus:status})
       };
     console.log(id,status);
     fetch('http://modelskit:8080/api_userRank',myInit)
-    .then((response)=>response.json())
-    .then(json=>console.log(json));
+    .then((response)=>{
+        if(response.ok){
+            return response.json()
+        }
+        else{
+            caller.checked=oldStatus;
+            sentFlash=true;
+            launchFlash(toastDetails.error,`Une erreur s'est produite ${response.statusText}`);
+            return response;
+        }
+    })
+    .then((json)=>{
+        if(json.result){
+            launchFlash(toastDetails.success,"La modification a bien été effectuée");
+        }
+        else{
+            caller.checked=oldStatus;
+            if(!sentFlash){
+                launchFlash(toastDetails.error,"Une erreur s'est produite");
+            }
+        }
+    });
 }
 
 const changeUserRole=(id,role)=>{
 
+}
+
+const launchFlash=(typeFlash,message)=>{
+    const flashContainer=document.createElement('ul');
+    flashContainer.classList.add('toast_notifications');
+    const toast=document.createElement('li');
+    toast.className=typeFlash.classname;
+    toast.classList.add('toast');
+    const divToast=document.createElement('div');
+    divToast.className="toast_column";
+    const icon=document.createElement('i');
+    icon.classList.add('fa-solid');
+    icon.classList.add(typeFlash.icon);
+    const spanMessage=document.createElement('span');
+    spanMessage.innerText=message;
+    divToast.appendChild(icon);
+    divToast.appendChild(spanMessage);
+    const closeIcon=document.createElement('i');
+    closeIcon.classList.add('fa-solid');
+    closeIcon.classList.add('fa-xmark');
+    closeIcon.addEventListener('click',()=>removeToast(toast));
+    toast.appendChild(divToast);
+    toast.appendChild(closeIcon);
+    flashContainer.appendChild(toast);
+    toast.timeouId=setTimeout(()=>removeToast(toast),toastDetails.timer);
+    document.body.appendChild(flashContainer);
 }

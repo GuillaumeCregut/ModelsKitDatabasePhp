3498ee330a46959ace087398ebba3d08a482e3c5
Guillaume Crégut <gcregut@free.fr>
2023-10-05 20:32:18 +0200

    [Feat]: Builder page : display builders OK, add builder OK

---
+++ public/assets/scripts/builders.js
@@ -0,0 +1,86 @@
+const form=document.getElementById('form-add');
+const closeModal=document.getElementById('close-modal');
+const updateBtns=document.querySelectorAll('.single-update-btn');
+const updateFormBtn=document.getElementById('update_single');
+const inputName=document.getElementById('newNameMod');
+const countrySelector=document.getElementById('countryBuilder');
+
+countrySelector.addEventListener('change',(e)=>{
+    if (parseInt(countrySelector.value)!==0){
+        countrySelector.classList.remove('wrong');
+    }
+})
+
+updateFormBtn.addEventListener('submit',(e)=>{
+    if (inputName.value===''){
+        e.preventDefault();
+    }
+})
+
+const newName=document.getElementById('new-name');
+if ( window.history.replaceState ) {
+    window.history.replaceState( null, null, window.location.href );
+}
+form.addEventListener('submit',(event)=>{
+    const countryId=parseInt(countrySelector.value);
+    if ((newName.value==='')||countryId===0){
+        event.preventDefault();
+        if(newName.value==='')
+            newName.classList.add('wrong');
+        if (countryId===0){
+            countrySelector.classList.add('wrong');
+        }
+    }
+});
+
+newName.addEventListener('keypress',()=>{
+        newName.classList.remove('wrong');
+})
+
+const showModal=(id, name)=>{
+    //Blur the page
+    const page=document.querySelector('.params-container');
+    page.classList.add('blured');
+    //Set values in modal form
+    inputName.value=name;
+    const inputHiddenId=document.getElementById('modSingle');
+    inputHiddenId.value=id;
+    //show the modal div
+    const modalDiv=document.querySelector('.singleModal')
+    modalDiv.classList.remove('modal-hidden');
+}
+
+const hideModal=()=>{
+    //UnBlur the page
+    const page=document.querySelector('.params-container');
+    page.classList.remove('blured');
+    //Hide model div
+    const modalDiv=document.querySelector('.singleModal');
+    modalDiv.classList.add('modal-hidden');
+}
+
+closeModal.addEventListener('click',hideModal);
+updateBtns.forEach((btn)=>{
+    const id=btn.dataset.id;
+    const name=btn.dataset.name;
+    btn.addEventListener('click',()=>showModal(id,name))
+});
+
+/*
+        Update this to adapt with builder
+const deleteBtn=document.querySelectorAll('.single-delete-btn');
+const formDelete=document.getElementById('form-delete-category');
+const idDelete=document.getElementById('id_hidden');
+
+deleteBtn.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        const id=btn.dataset.id;
+        const name=btn.dataset.name;
+        if(confirm(`Voulez-vous supprimer la catégorie ${name} ?`)){
+            idDelete.value=id;
+            formDelete.submit();
+        }
+    })
+})
+
+*/

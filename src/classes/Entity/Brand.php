3ded2433a187ab97fc0429e443512cfba061a602
Guillaume Crégut <gcregut@free.fr>
2023-10-05 17:52:27 +0200

    [Feat]: All Brand: create CRUD and page for period

---
+++ src/classes/Entity/Brand.php
@@ -0,0 +1,53 @@
+<?php
+namespace Editiel98\Entity;
+
+use Editiel98\Factory;
+use Editiel98\Manager\BrandManager;
+
+class Brand extends Entity{
+    private string $name;
+    private int $id;
+    private BrandManager $manager;
+
+    public function __construct()
+    {
+        $this->manager=Factory::getManager('brandManager');
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id=$id;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name=$name;
+        return $this;
+    }
+
+    public function save(): bool|int
+    {
+       return $this->manager->save($this);
+    }
+
+    public function delete(): bool|int
+    {
+        return $this->manager->delete($this);
+    }
+
+    public function update(): bool|int
+    {
+        return $this->manager->update($this);
+    }
+}

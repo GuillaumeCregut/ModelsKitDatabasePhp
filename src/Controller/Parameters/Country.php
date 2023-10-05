0ee49651920a545db683e595d44af91aeb9dee3e
Guillaume Crégut <gcregut@free.fr>
2023-10-05 13:41:47 +0200

    [Feat]: countryController: create function to display countries and add

--- src/Controller/Parameters/Country.php
+++ src/Controller/Parameters/Country.php
@@ -1,20 +1,94 @@
 <?php
 namespace App\Controller\Parameters;
 
+use Editiel98\App;
+use Editiel98\Entity\Country as EntityCountry;
 use Editiel98\Factory;
+use Editiel98\Manager\CountryManager;
+use Editiel98\Router\Controller;
 
-class Country
+class Country extends Controller
 {
-    private $smarty;
-
-    public function __construct($smarty)
-    {
-        $this->smarty=Factory::getSmarty();
-    }
-
     public function render()
     {
-        echo "Country";
-        var_dump($_POST);
+        if(!empty($_POST)){
+            var_dump($_POST);
+            if(!$this->usePost())
+            {
+                //Une erreur s'est produite;
+            }
+        }
+        $countryManager=new CountryManager($this->dbConnection);
+        $countries= $countryManager->getAll();
+        //var_dump($countries);
+        if($this->isConnected){
+            $this->smarty->assign('connected',true);
+            if(App::ADMIN===$this->userRank){
+                $this->smarty->assign('isAdmin',true);
+            }
+        }
+        $this->smarty->assign('list',$countries);
+        $this->smarty->assign('params','params');
+        $this->smarty->assign('country_menu','params');
+        $this->smarty->display('params/countries.tpl');
     }
+
+    private function usePost(): bool{
+        if(isset($_POST['action'])){
+            switch ($_POST['action']){
+                case "add" :
+                    if(isset($_POST['name'])){
+                        $name=htmlspecialchars($_POST['name'], ENT_NOQUOTES, 'UTF-8');
+                        return $this->add($name);
+                    }
+                    else
+                        return false;
+                    break;
+                case "remove":
+                    if(isset($_POST['id'])){
+                        $id=intval($_POST['id']);
+                        return $this->remove($id);
+                    }
+                    else
+                        return false;
+                    break;
+                case "update":
+                    if(isset($_POST['name'])){
+                        $name=htmlspecialchars($_POST['name'], ENT_NOQUOTES, 'UTF-8');
+                    }
+                    else
+                        return false;
+                    if(isset($_POST['id'])){
+                        $id=intval($_POST['id']);
+                    }
+                    else
+                        return false; 
+                    return $this->update($id,$name);   
+                    break;
+                default:
+                    return false;
+            }
+        }
+    }
+
+    private function add(string $name): bool 
+    {
+        $country=new EntityCountry();
+        $country->setName($name);
+        $test=$country->save();
+        var_dump($test);
+        return false;
+    }
+
+    private function remove(int $id): bool 
+    {
+        return false;
+    }
+
+    private function update(int $id, string $name): bool 
+    {
+        return false;
+    }
+
+    
 }

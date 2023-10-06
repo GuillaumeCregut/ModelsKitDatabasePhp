e2e3a71d5b541cc31cf931c84ba886572d7605da
Guillaume Crégut <gcregut@free.fr>
2023-10-06 16:38:03 +0200

    [Feat]: Model controller: display list of models with user likes

--- src/Controller/Parameters/Model.php
+++ src/Controller/Parameters/Model.php
@@ -2,25 +2,50 @@
 namespace App\Controller\Parameters;
 
 use Editiel98\App;
+use Editiel98\Entity\User;
 use Editiel98\Manager\BrandManager;
 use Editiel98\Manager\BuilderManager;
 use Editiel98\Manager\CategoryManager;
 use Editiel98\Manager\CountryManager;
+use Editiel98\Manager\ModelManager;
 use Editiel98\Manager\PeriodManager;
 use Editiel98\Manager\ScaleManager;
 use Editiel98\Router\Controller;
+use Editiel98\Session;
 
 class Model extends Controller
 {
 
     public function render()
     {
+        if(!empty($_POST))
+            var_dump($_POST);
         if($this->isConnected){
             $this->smarty->assign('connected',true);
             if(App::ADMIN===$this->userRank){
                 $this->smarty->assign('isAdmin',true);
             }
         }
+        $modelManager=new ModelManager($this->dbConnection);
+        $models=$modelManager->getAll();
+        if($this->isConnected){
+            $this->smarty->assign('connected',true);
+            if(App::ADMIN===$this->userRank){
+                $this->smarty->assign('isAdmin',true);
+            }
+            $userId=$this->session->getKey(Session::SESSION_USER_ID);
+            $user=new User();
+            $user->setId($userId);
+            $favorite=$user->getFavorite();
+            foreach($models as $model){
+                $id=$model->getId();
+                if(in_array($id,$favorite)){
+                    $model->setLiked(true);
+                }
+            }
+        }      
+        
+        //var_dump($models);
         //Get all needed datas for create a model
         $builderManager=new BuilderManager($this->dbConnection);
         $builders=$builderManager->getAll();
@@ -35,7 +60,7 @@ class Model extends Controller
         $countryManager=new CountryManager($this->dbConnection);
         $countries=$countryManager->getAll();
 
-        $this->smarty->assign('list','');
+        $this->smarty->assign('list',$models);
         $this->smarty->assign('countries',$countries);
         $this->smarty->assign('categories',$categories);
         $this->smarty->assign('periods',$periods);

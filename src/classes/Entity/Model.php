dc39673d1b2c265cac26b6e131a5f7a22ddcaad8
Guillaume Crégut <gcregut@free.fr>
2023-10-06 16:36:28 +0200

    [Feat]: Model: create entity and manager

---
+++ src/classes/Entity/Model.php
@@ -0,0 +1,185 @@
+<?php
+namespace Editiel98\Entity;
+
+use Editiel98\Factory;
+use Editiel98\Manager\ModelManager;
+
+class Model extends Entity{
+    private string $name;
+    private int $id;
+    private string $buildername;
+    private string $countryname;
+    private string $categoryname;
+    private string $periodname;
+    private string | null $scalemates;
+    private string $reference;
+    private string $scalename;
+    private string $brandname;
+    private bool $liked=false;
+    private string | null $picture;
+
+    private int $builder;
+    private int $countryid;
+    private int $category;
+    private int $period;
+    private int $scale;
+    private int $brand;
+
+    private ModelManager $manager;
+
+    public function __construct()
+    {
+        $this->manager=Factory::getManager('modelManager');
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id=$id;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name=$name;
+        return $this;
+    }
+    public function getBrand(): string
+    {
+        return $this->brandname;
+    }
+
+    // public function set(string $name): self
+    // {
+    //     $this->=$name;
+    //     return $this;
+    // }
+
+    public function getBuilder():  string
+    {
+        return $this->buildername;
+    }
+
+    // public function setBuilder(): self
+    // {
+    //     $this->=;
+    //     return $this;
+    // }
+
+    public function getCountryName(): string
+    {
+        return $this->countryname;
+    }
+
+    // public function set(): self
+    // {
+    //     $this->=;
+    //     return $this;
+    // }
+
+    public function getCategory(): string 
+    {
+        return $this->categoryname;
+    }
+
+    // public function set(): self
+    // {
+    //     $this->=;
+    //     return $this;
+    // }
+
+    public function getPeriod(): string 
+    {
+        return $this->periodname;
+    }
+
+    // public function set(): self
+    // {
+    //     $this->=;
+    //     return $this;
+    // }
+
+    public function getScalemates(): string |null
+    {
+        return $this->scalemates;
+    }
+
+    public function setScalemates(string $link): self
+    {
+        $this->scalemates=$link;
+        return $this;
+    }
+
+    public function getRef(): string 
+    {
+        return $this->reference;
+    }
+
+    public function setRef(string $ref): self
+    {
+        $this->reference=$ref;
+        return $this;
+    }
+
+    public function getScale(): string 
+    {
+        return $this->scalename;
+    }
+
+
+    // public function set(): self
+    // {
+    //     $this->=;
+    //     return $this;
+    // }
+
+    public function getLiked(): bool | null
+    {
+        return $this->liked;
+    }
+
+
+    public function setliked(bool $liked): self
+    {
+        $this->liked=$liked;
+        return $this;
+    }
+
+    public function getImage(): string |null
+    {
+        return $this->picture;
+    }
+
+
+    public function setImage($image): self
+    {
+        $this->picture=$image;
+        return $this;
+    }
+
+    //Have to do getter/setter or ids
+
+    public function save(): bool|int
+    {
+       return $this->manager->save($this);
+    }
+
+    public function delete(): bool|int
+    {
+        return $this->manager->delete($this);
+    }
+
+    public function update(): bool|int
+    {
+        return $this->manager->update($this);
+    }
+}
---
+++ src/classes/Manager/ModelManager.php
@@ -0,0 +1,165 @@
+<?php
+namespace Editiel98\Manager;
+
+
+use Editiel98\Database\Database;
+use Editiel98\DbException;
+use Editiel98\Entity\Entity;
+use Editiel98\Entity\Model;
+use Editiel98\Event\Emitter;
+use Editiel98\Flash;
+
+/**
+ * Manage single entity to DB
+ */
+class ModelManager extends Manager implements ManagerInterface
+{
+    public function __construct(Database $db)
+    {
+        $this->db=$db;
+        $this->table='model';
+        $this->className='Editiel98\Entity\Model';
+    }
+
+    
+    /**
+     * Get all datas from DB for the entity
+     *
+     * @return array
+     */
+    public function getAll():array
+    {
+        $query="SELECT id,name, builder, category, brand, period, scale, reference, picture, scalemates,
+        buildername, countryid, categoryname, brandname, periodname, scalename, countryname  
+        FROM model_full"; 
+        try{
+            $result=$this->db->query($query,$this->className); 
+            return $result;
+        }
+        catch(DbException $e){
+            $this->loadErrorPage($e->getdbMessage());
+        }
+    }
+
+    /**
+     * Find an entity in DB by Id
+     *
+     * @param integer $id
+     * @return object : class of the entity
+     */
+    public function findById(int $id): Entity
+    {
+        // $query='SELECT id, name FROM ' . $this->table.' WHERE id=:id';
+        // $result=$this->prepareSQL($query,[':id'=>$id],true); 
+        // return $result;
+        return new Model();
+    }
+
+    /**
+     * Return entity from DB by name
+     *
+     * @param string $name
+     * @return object class of the entity
+     */
+    public function findByName(string $name): Model
+    {
+        $query='SELECT id, name FROM ' . $this->table .' WHERE name=:name';
+        $result=$this->prepareSQL($query,[':name'=>$name],true); 
+        return $result;
+    }
+
+    /**
+     * Update an entity in DB
+     *
+     * @param Buidler $entity : entity to update
+     * @return boolean
+     */
+    public function update(Entity $entity): bool{
+        // $query='UPDATE ' . $this->table .' SET name=:name, country=:country WHERE id=:id';
+        // $name=$entity->getName();
+        // $id=$entity->getId();
+        // $country=$entity->getCountryId();
+        // $values=['name'=>$name,':id'=>$id,':country'=>$country];
+        // return $this->execSQL($query,$values);
+        //Debug
+        return false;
+    }
+
+    /**
+     * Store Entity in Database
+     *
+     * @param Builder $entity : Entity to store in DB
+     * @return boolean
+     */
+    public function save(Entity $entity): bool{
+        // $query='INSERT INTO ' . $this->table . '(name, country) VALUES (:name,:country)';
+        // $name=$entity->getName();  
+        // $country=$entity->getCountryId();
+        // $result=$this->execSQL($query,['name'=>$name,':country'=>$country]);
+        // return $result;
+        return false;
+    }
+
+    /**
+     * Delete an entity into DB
+     *
+     * @param Builder $entity : Entity to delete
+     * @return boolean
+     */
+    public function delete(Entity $entity) : bool{
+        // $query='DELETE FROM ' . $this->table . ' WHERE id=:id';
+        // $id=$entity->getId();
+        // $result=$this->execSQL($query,['id'=>$id]);
+        // return $result;
+        return false;
+    }
+
+    /**
+     * Exec the query
+     *
+     * @param string $query : Query to execute
+     * @param array $vars : vars for the query
+     * @return mixed
+     */
+    private function execSQL(string $query,array $vars): mixed
+    {
+        try{
+            $result=$this->db->exec($query,$vars);
+            return $result;
+        }
+        catch(DbException $e){
+            if($e->getDbCode()===23000){
+                $flash=new Flash();
+                $flash->setFlash('Modification impossible','error');
+                return false;
+            }
+            $message='SQL : ' . $query .'a poser problème';
+            $emitter=Emitter::getInstance();
+            $emitter->emit(Emitter::DATABASE_ERROR,$message);
+            $this->loadErrorPage($e->getdbMessage());
+        }
+
+    }
+
+    /**
+     * Execute a prepared query
+     *
+     * @param string $query : query to execute
+     * @param array $vars : vars for the query
+     * @param boolean $single : return one result or not
+     * @return mixed
+     */
+    private function prepareSQL(string $query, array $vars, bool $single): mixed
+    {
+        try{
+            $result=$this->db->prepare($query,$this->className,$vars,$single);
+            return $result;
+        }
+        catch(DbException $e){
+            $message='SQL : ' . $query .'a poser problème';
+            $emitter=Emitter::getInstance();
+            $emitter->emit(Emitter::DATABASE_ERROR,$message); 
+            $this->loadErrorPage($e->getMessage());
+        }
+    }
+}

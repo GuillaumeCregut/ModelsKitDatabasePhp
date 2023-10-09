243cf1d0ff7679705014f3e143829ba8662772cf
Guillaume Crégut <gcregut@free.fr>
2023-10-09 19:34:05 +0200

    [Feat]: Info controller: add update user info method

--- src/Controller/Profil/Info.php
+++ src/Controller/Profil/Info.php
@@ -19,7 +19,6 @@ class Info extends Controller
         }
         //todo
         if(!empty($_POST)){
-            var_dump($_POST);
             $template=$this->usePost();
         }
         else $template='profil/info.tpl';
@@ -46,6 +45,113 @@ class Info extends Controller
 
     private function updateUser(): bool
     {
+        $this->getUser();
+        $changeUser=false;
+        //Check POST values
+        if(isset($_POST['firstname'])){
+            $firstname=htmlspecialchars($_POST['firstname'], ENT_NOQUOTES, 'UTF-8');
+        }
+        else{
+            $firstname='';
+        }
+        if(isset($_POST['lastname'])){
+            $lastname=htmlspecialchars($_POST['lastname'], ENT_NOQUOTES, 'UTF-8');
+        }
+        else{
+            $lastname='';
+        }
+        if(isset($_POST['login'])){
+            $login=htmlspecialchars($_POST['login'], ENT_NOQUOTES, 'UTF-8');
+        }
+        else{
+            $login='';
+        }
+        if(isset($_POST['email'])){
+            $email=htmlspecialchars($_POST['email'], ENT_NOQUOTES, 'UTF-8');
+        }
+        else{
+            $email='';
+        }
+        if(isset($_POST['password'])){
+            $password=htmlspecialchars($_POST['password'], ENT_NOQUOTES, 'UTF-8');
+        }
+        else{
+            $password='';
+        }
+        if(isset($_POST['isvisible'])){
+            $isVisible=$_POST['isvisible'];
+           if($isVisible==='on'){
+            $isVisible=true;
+           }
+           else $isVisible=false;
+        }
+        else{
+            $isVisible=false;
+        }
+        if(isset($_POST['allow'])){
+            $allow=$_POST['allow'];
+            if($allow==='on'){
+                $allow=true;
+            }else $allow =false;
+        }
+        else{
+            $allow=false;
+        }
+        if(isset($_POST['change-pass'])){
+            $changePass=$_POST['change-pass'];
+            if($changePass==='on'){
+                $changePass=true;
+            }else $changePass=false;
+        }
+        else{
+            $changePass=false;
+        }
+        //Check Users values
+        if($this->user->getFirstname()!==$firstname){
+            if($firstname!==''){
+                $this->user->setFirstname($firstname);
+                $changeUser=true;
+            }
+        }
+        if($this->user->getLastname()!==$lastname){
+            if($lastname!==''){
+                $this->user->setLastname($lastname);
+                $changeUser=true;
+            }
+        }
+        if($this->user->getLogin()!==$login){
+            if($login!==''){
+                $this->user->setLogin($login);
+                $changeUser=true;
+            }
+        }
+        if($this->user->getEmail()!==$email){
+            if($email!==''){
+                $this->user->setEmail($email);
+                $changeUser=true;
+            }
+        }
+        if($password!='' && $changePass){
+            $this->user->setPassword($password);
+            $changeUser=true;
+        }
+        if($this->user->getVisible()!==$isVisible){
+           $this->user->setVisible($isVisible);
+           $changeUser=true;
+        }
+        if($this->user->getAllow()!==$allow){
+            $this->user->setAllow($allow);
+            $changeUser=true;
+        }
+        //Check avatar
+        $fileOK=$this->storeFile($this->user->getId());
+        if($fileOK!==''){
+                $this->user->setAvatar($fileOK);
+                $changeUser=true;
+        }
+        if($changeUser){
+            $this->user->update();
+        }
         return false;
     }
 
@@ -69,6 +175,30 @@ class Info extends Controller
         $this->smarty->assign('info_menu','profil');
         $this->smarty->assign('user',$this->user);
         $this->smarty->display($template);
+    }
+
+    private function storeFile(int $idUser): string
+    {
+        $filename='';
+        $baseDir='/public/assets/uploads/users/'. $idUser .'/';
+        if(isset($_FILES['avatar'])){
+            $image=$_FILES['avatar'];
+            if ($image['error'] == UPLOAD_ERR_OK){ 
+                $type=$image['type'];
+                if($type==='image/jpeg' || $type==='image/png'){
+                    $ext=explode('/',$type)[1];
+                    if($image['size']<=500*1000){
+                        $uploadDir=dirname(dirname(dirname(__DIR__))) . $baseDir;
+                        $destFile=$uploadDir . 'avatar.' . $ext;
+                        $fileOK=move_uploaded_file($image['tmp_name'],$destFile);
+                        if($fileOK){
+                            $filename='avatar.' . $ext;
+                        }
+                    }
+                }
+            }
+        }
+        return $filename;
     }
 
 }

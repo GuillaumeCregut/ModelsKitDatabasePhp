ab6851d18a43dc8f257effd8b7a2886eb0b53771
Guillaume Crégut <gcregut@free.fr>
2023-10-12 21:57:31 +0200

    [Feat]: Order page: add order lines

--- public/assets/scripts/invoices.js
+++ public/assets/scripts/invoices.js
@@ -2,12 +2,27 @@ const detailBtns=document.querySelectorAll('.detail-btn');
 const HideOrdersBtn=document.getElementById('order-show-btn');
 const showNewModelModalBtn=document.querySelector('.new-model-add');
 const bodyOrder=document.getElementById('order-list-body');
-const test=document.getElementById('test');
+const formAddOrder=document.getElementById('new-order-form');
+const refOrder=document.getElementById('new-ref');
+const orderSupplier=document.getElementById('new-provider');
+const dateOrder=document.getElementById('new-date');
+
 
 let mypopup=null;
 let modelPopup=null;
 let isOrdersShown=false;
 let modelInArray=[];
+const toastDetails={
+    timer: 5000,
+    success:{
+        icon: "fa-circle-check",
+        classname: "toast_success"
+    },
+    error:{
+        icon: "fa-circle-xmark",
+        classname:"toast_error"
+    }
+}
 
 const openDetailPopup=(ref,key)=>{
     if(mypopup==null || mypopup.closed){
@@ -17,6 +32,8 @@ const openDetailPopup=(ref,key)=>{
             `popup,
             top=50px,
             left=100px,
+            width=640px,
+            height=480px,
             toolbar=no,
             status=no,
             menubar=no,
@@ -56,42 +73,31 @@ const removeRow=(id)=>{
     })
     if(removeRowSel){
         bodyOrder.removeChild(removeRowSel);
-        //Remove form array
         const tempArray=modelInArray.filter((idarray)=>idarray!==id);
         modelInArray=tempArray;
-        console.log(modelInArray);
     }
 }
 
 const addQttyModel=(id,qty)=>{
-    //Find Span Qtty with Id;
     const spansQtty=document.querySelectorAll('.span-qtty-model');
     let span=null;
     spansQtty.forEach((spanQty)=>{
-        console.log(spanQty);
         if(parseInt(spanQty.dataset.id)===id){
             span=spanQty;
         }
     })
-    console.log(span);
-    //Get value
     const oldQty=parseInt(span.textContent);
     const newQty=oldQty+qty;
-    //If value ==0 delete row
     if(newQty<=0){
-        /*todo*/
         removeRow(id);
     }
-    //Change Value
     else{
         span.textContent=newQty;
-        console.log('change qtty',id,qty)
     }
 }
 
 
 function addModelToList(model){
-    //Check if model added is in table before
     if(modelInArray.includes(model.id)){
         addQttyModel(model.id,model.qty);
         return;
@@ -101,8 +107,17 @@ function addModelToList(model){
     newRow.classList.add('new-order-row-item');
     newRow.dataset.id=model.id;
     const newName=document.createElement('td');
+    const inputHidden=document.createElement('input');
+    inputHidden.setAttribute("type","hidden");
+    inputHidden.setAttribute('name','lines[]');
+    const valueInput=`${model.id};${model.price};${model.qty}`;
+    inputHidden.setAttribute("value",valueInput);
+    inputHidden.className="hidden-line-values";
+    newName.appendChild(inputHidden);
     newName.classList.add('orders-cell');
-    newName.textContent=model.name;
+    const newNameSpan=document.createElement('span');
+    newNameSpan.textContent=model.name;
+    newName.appendChild(newNameSpan);
     newRow.appendChild(newName);
     const newBrand=document.createElement('td');
     newBrand.classList.add('orders-cell');
@@ -134,7 +149,6 @@ function addModelToList(model){
     addQtyBtn.addEventListener('click',()=>{
         addQttyModel(model.id,1);
     })
-    //to do =>action
     addQtyBtn.dataset.id=model.id;
     newQtty.appendChild(addQtyBtn);
     newRow.appendChild(newQtty);
@@ -142,10 +156,7 @@ function addModelToList(model){
     newPrice.classList.add('orders-cell');
     newPrice.textContent=model.price;
     newRow.appendChild(newPrice);
-    //Add those info to form
-    /*todo*/
     bodyOrder.appendChild(newRow);
-    console.log(model);
 }
 
 
@@ -167,4 +178,46 @@ showNewModelModalBtn.addEventListener('click',()=>{
     }else{
         modelPopup.focus();
     }
+});
+
+
+formAddOrder.addEventListener('submit',(e)=>{
+    const supplierValue=orderSupplier.value;
+    const dateValue=dateOrder.value;
+    const refValue=refOrder.value;
+    const orderLines=document.querySelectorAll('.hidden-line-values');
+    if(orderLines.length===0 || refValue==='' || supplierValue==0 || dateOrder===''){
+        e.preventDefault();
+        launchFlash(toastDetails.error,'Veuillez remplir tous les champs');
+    }
 })
+
+const launchFlash=(typeFlash,message)=>{
+    if(document.getElementsByClassName('toast_notifications').length===0)
+    {
+        const flashContainer=document.createElement('ul');
+        flashContainer.classList.add('toast_notifications');
+        document.body.appendChild(flashContainer);
+    }
+    flashContainer=document.getElementsByClassName('toast_notifications')[0];
+    const toast=document.createElement('li');
+    toast.className=typeFlash.classname;
+    toast.classList.add('toast');
+    const divToast=document.createElement('div');
+    divToast.className="toast_column";
+    const icon=document.createElement('i');
+    icon.classList.add('fa-solid');
+    icon.classList.add(typeFlash.icon);
+    const spanMessage=document.createElement('span');
+    spanMessage.innerText=message;
+    divToast.appendChild(icon);
+    divToast.appendChild(spanMessage);
+    const closeIcon=document.createElement('i');
+    closeIcon.classList.add('fa-solid');
+    closeIcon.classList.add('fa-xmark');
+    closeIcon.addEventListener('click',()=>removeToast(toast));
+    toast.appendChild(divToast);
+    toast.appendChild(closeIcon);
+    flashContainer.appendChild(toast);
+    toast.timeouId=setTimeout(()=>removeToast(toast),toastDetails.timer);
+}

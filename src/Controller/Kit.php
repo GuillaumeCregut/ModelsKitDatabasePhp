d5085088a1fe75ee4b9abe440dc77a411a3c7796
Guillaume Crégut <gcregut@free.fr>
2023-10-13 11:39:05 +0200

    [Feat]: Kit Controller: add route to general(management)

--- src/Controller/Kit.php
+++ src/Controller/Kit.php
@@ -7,7 +7,37 @@ class Kit extends Controller{
 
     public function render()
     {
-        $this->smarty->assign('kits','kits');
-        $this->smarty->display('kit/index.tpl');
+        if (empty($this->subPages)) {
+            $this->smarty->assign('kits','kits');
+            $this->smarty->display('kit/index.tpl');
+        }else{
+            switch ($this->subPages[0]) {
+                case'general':
+                    $className='Management';
+                    break;
+                case 'commandes':
+                    $className='';
+                    break;
+                case 'stock':
+                    $className='';
+                    break;
+                case 'wip':
+                    $className='';
+                    break;
+                case 'choose':
+                    $className='';
+                    break;
+                case 'finis':
+                    $className='';
+                    break;
+                default: 
+                    $page=new Error('404');
+                    $page->render();
+                    die();
+            }
+            $classPage = 'App\\Controller\\Kit\\' . $className;
+            $page=new $classPage([],$this->params);
+            $page->render();
+        }
     }
 }

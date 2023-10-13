f9ffbcb8c4d4bb390b6044456580abebdc3b39e5
Guillaume Crégut <gcregut@free.fr>
2023-10-13 18:50:56 +0200

    [Feat]: Management contoller: display page without models

--- src/Controller/Kit/Management.php
+++ src/Controller/Kit/Management.php
@@ -1,18 +1,59 @@
 <?php
 namespace App\Controller\Kit;
 
+use Editiel98\App;
+use Editiel98\Entity\User;
 use Editiel98\Router\Controller;
+use Editiel98\Session;
 
 class Management extends Controller
 {
+    private array $liked=[];
+    private array $buy=[];
+    private array $stock=[];
+    private array $wip=[];
+    private array $finished=[];
     public function render()
     {
+        $userId=$this->session->getKey(Session::SESSION_USER_ID);
+        $user=new User();
+        $user->setId($userId);
+        $models=$user->getModels();
+        $this->parseModels($models);
         $this->displayPage();
     }
 
     private function displayPage()
     {
+        $this->smarty->assign('zoneLike', App::STATE_LIKED);
+        $this->smarty->assign('zoneBuy',App::STATE_BUY);
+        $this->smarty->assign('zoneStock',App::STATE_STOCK);
+        $this->smarty->assign('zoneWip',App::STATE_WIP);
+        $this->smarty->assign('zoneFinished',App::STATE_FINISHED);
         $this->smarty->assign('stock_menu',true);
         $this->smarty->display('kit/management.tpl');
+    }
+
+    private function parseModels(array $models): void
+    {
+        foreach($models as $model){
+            switch($model->state){
+                case App::STATE_LIKED:
+                    $this->liked[]=$model;
+                    break;
+                case App::STATE_BUY:
+                    $this->buy[]=$model;
+                    break;
+                case App::STATE_STOCK:
+                    $this->stock[]=$model;
+                    break;
+                case App::STATE_WIP:
+                    $this->wip[]=$model;
+                    break;
+                case App::STATE_FINISHED:
+                    $this->finished[]=$model;
+                    break;
+            }
+        }
     }
 }

54a0f822cfcf8694750b0de77e8bbef21280ee21
Guillaume Crégut <gcregut@free.fr>
2023-10-13 18:47:09 +0200

    [Feat]: Management page: add drag and drop function

---
+++ public/assets/scripts/management.js
@@ -0,0 +1,114 @@
+const modelsCards=document.querySelectorAll('.draggable');
+const dropZones=document.querySelectorAll('.dropzone');
+const dropAllow=['liked-kits','ordered','stock','wip','finished'];
+
+const toastDetails={
+    timer: 5000,
+    success:{
+        icon: "fa-circle-check",
+        classname: "toast_success"
+    },
+    error:{
+        icon: "fa-circle-xmark",
+        classname:"toast_error"
+    }
+}
+
+function allowDrop(e)
+{
+    e.preventDefault();
+    const idReceive=e.target.id;
+    if(!dropAllow.includes(idReceive)){
+        return;
+    }
+    e.target.classList.add('dropOK');
+}
+
+
+modelsCards.forEach((card)=>{
+    card.addEventListener('dragstart',(e)=>{
+        drag(e);
+    });
+});
+
+dropZones.forEach((dropzone)=>{
+    dropzone.addEventListener('dragover',(e)=>{
+        allowDrop(e);
+    });
+    dropzone.addEventListener('drop',(e)=>{
+        drop(e);
+    })
+    dropzone.addEventListener('dragleave',(e)=>{
+        dragLeave(e);
+    });
+})
+
+function drag(e)
+{
+   e.dataTransfer.setData('text',e.target.id);
+}
+
+
+const dragLeave=(e)=>{
+    if(e.target.classList.contains('dropOK')){
+        e.target.classList.remove('dropOK');
+    }
+}
+
+function drop(e){
+    e.preventDefault();
+    const idReceive=e.target.id;
+    const newState=e.target.dataset.id;
+    if(!dropAllow.includes(idReceive)){
+        return;
+    }
+    let data=e.dataTransfer.getData("text");
+    const card=document.getElementById(data);
+    const idCard=card.dataset.id;
+    console.log(`id:${idCard}, new state: ${newState}, carré: ${idReceive}`);
+    const result=sendUpdateState(idCard,newState);
+    e.target.classList.remove('dropOK');
+    if(result){
+        try{
+            e.target.appendChild(card);
+        }
+        catch(e){}
+    }
+    else{
+        launchFlash(toastDetails.error,'Une erreur est survenue');
+    }
+}
+
+const sendUpdateState=(id,state)=>{
+    return true;
+}
+
+const launchFlash=(typeFlash,message)=>{
+    if(document.getElementsByClassName('toast_notifications').length===0)
+    {
+        const flashContainer=document.createElement('ul');
+        flashContainer.classList.add('toast_notifications');
+        document.body.appendChild(flashContainer);
+    }
+    flashContainer=document.getElementsByClassName('toast_notifications')[0];
+    const toast=document.createElement('li');
+    toast.className=typeFlash.classname;
+    toast.classList.add('toast');
+    const divToast=document.createElement('div');
+    divToast.className="toast_column";
+    const icon=document.createElement('i');
+    icon.classList.add('fa-solid');
+    icon.classList.add(typeFlash.icon);
+    const spanMessage=document.createElement('span');
+    spanMessage.innerText=message;
+    divToast.appendChild(icon);
+    divToast.appendChild(spanMessage);
+    const closeIcon=document.createElement('i');
+    closeIcon.classList.add('fa-solid');
+    closeIcon.classList.add('fa-xmark');
+    closeIcon.addEventListener('click',()=>removeToast(toast));
+    toast.appendChild(divToast);
+    toast.appendChild(closeIcon);
+    flashContainer.appendChild(toast);
+    toast.timeouId=setTimeout(()=>removeToast(toast),toastDetails.timer);
+}
--- src/templates/kit/management.tpl
+++ src/templates/kit/management.tpl
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="assets/styles/kit/management.css">
 {/block}
 {block name=script}
-<script src="assets/scripts/management.js"></script>
+<script src="assets/scripts/management.js" defer></script>
 {/block}
 {block name=innerMenu}
     <div class="main-kit-container">
@@ -13,31 +13,40 @@
         <div class="kits-management-container">
             <section class="kits-container">
                 <h3>Modèles likés</h3>
-                <div class="dropzone" id="liked-kits">
+                <div class="dropzone" id="liked-kits" data-id="{$zoneLike}">
 
                 </div>
             </section>
             <section class="kits-container">
                 <h3>Modèles commandés</h3>
-                <div class="dropzone" id="ordered">
+                <div class="dropzone" id="ordered" data-id="{$zoneBuy}">
                     
                 </div>
             </section>
             <section class="kits-container">
                 <h3>Modèles en stock</h3>
-                <div class="dropzone" id="stock">
-                    
+                <div class="dropzone" id="stock" data-id="{$zoneStock}">
+                    <article class="kit-card draggable" id="model_2" draggable="true" data-id="2">
+                        <h4>Nom2 - Builder</h4>
+                        <p>Marque - Echelle</p>
+                        <p>Référence</p>
+                    </article>
                 </div>
             </section>
             <section class="kits-container">
                 <h3>Modèles en cours</h3>
-                <div class="dropzone" id="wip">
-                    
+                <div class="dropzone" id="wip" data-id="{$zoneWip}">
+                    {* Pour article, mettre le Id en dynamique avec Id du modele*}
+                    <article class="kit-card draggable" id="model_1" draggable="true" data-id="1">
+                        <h4>Nom - Builder</h4>
+                        <p>Marque - Echelle</p>
+                        <p>Référence</p>
+                    </article>
                 </div>
             </section>
             <section class="kits-container">
                 <h3>Modèles terminés</h3>
-                <div class="dropzone" id="finished">
+                <div class="dropzone" id="finished" data-id="{$zoneFinished}">
                     
                 </div>
             </section>

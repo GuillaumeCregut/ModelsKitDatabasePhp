9a1fd0478623cc35bdf0ea03666381d76e4a84c8
Guillaume Crégut <gcregut@free.fr>
2023-10-15 16:50:27 +0200

    [Style]: index: remmove comments

--- public/index.php
+++ public/index.php
@@ -1,22 +1,10 @@
 <?php
 use Editiel98\Autoloader;
 use Editiel98\App;
-use Editiel98\Auth\DbAuth;
 
 session_start();
 require '../src/classes/Autoloader.php';
 Autoloader::register();
 $app=new App();
-$app->run();  //remettre
-//En dessous à supprimer
-// $dbAuth=new DbAuth();
-// try{
-// $user=$dbAuth->login('gcregut','MUSTANG');
-// }
-// catch(PDOException $e){
-//     echo "titi";
-// }
-
-
-//Fin de suppression
+$app->run();  
 ?>

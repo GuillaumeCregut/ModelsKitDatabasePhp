7395a09b8f54b5971d72127e3f4894cf320c0392
Guillaume Crégut <gcregut@free.fr>
2023-10-15 19:36:57 +0200

    [Feat]: finisheddetails Controller: get model and messages

--- src/Controller/Kit/FinishedDetails.php
+++ src/Controller/Kit/FinishedDetails.php
@@ -1,12 +1,66 @@
 <?php
 namespace App\Controller\Kit;
 
+use App\Controller\Error;
+use Editiel98\Manager\MessageManager;
+use Editiel98\Manager\ModelManager;
 use Editiel98\Router\Controller;
+
 
 class FinishedDetails extends Controller
 {
+    private object $model;
+    private array $messages=[];
     public function render()
     {
-        
+        if (!$this->isConnected || !isset($_GET['id'])) {
+            //Render antoher page and die
+            $this->smarty->assign('profil', 'profil');
+            $this->smarty->display('profil/notconnected.tpl');
+            die();
+        }
+        //get id from GET
+        if (!$this->isConnected || !isset($_GET['id'])) {
+            //Render antoher page and die
+            $this->smarty->assign('profil', 'profil');
+            $this->smarty->display('profil/notconnected.tpl');
+            die();
+        }
+        $id=intval($_GET['id']);
+        if($id===0){
+            $page=new Error('404', 'le kit n\'existe pas');
+            $page->render();
+            die();
+        }
+        //Get model
+        $modelManager=new ModelManager($this->dbConnection);
+        $model=$modelManager->getOneFullById($id,$this->userId);
+        if(!$model){
+            $page=new Error('404', 'le kit n\'existe pas');
+            $page->render();
+            die();
+        }
+        $this->model=$model;
+        $dirPictures=$model->pictures;
+        if($dirPictures){
+            //Get pictures if there are
+
+        }
+        //get messages
+        $messageManager=new MessageManager($this->dbConnection);
+        $messages=$messageManager->getMessagesForModel($id);
+        $this->messages=$messages;
+        var_dump($model);
+        $this->displayPage();
+    }
+
+    private function displayPage()
+    {
+        $this->smarty->assign('kits', true);
+        $this->smarty->assign('model',$this->model);
+        if(!empty($this->messages)){
+            $this->smarty->assign('messages',$this->messages);
+        }
+        $this->smarty->display('kit/finishedDetails.tpl');
     }
 }

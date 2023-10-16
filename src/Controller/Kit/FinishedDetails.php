68789eaeecb4fe0dc6b28237b845774c153134a7
Guillaume Crégut <gcregut@free.fr>
2023-10-16 18:05:19 +0200

    [Feat]: finished details Controller: add function to remove picture

--- src/Controller/Kit/FinishedDetails.php
+++ src/Controller/Kit/FinishedDetails.php
@@ -12,6 +12,7 @@ class FinishedDetails extends Controller
     private object $model;
     private array $messages=[];
     private array $files=[];
+
     public function render()
     {
         if (!$this->isConnected || !isset($_GET['id'])) {
@@ -33,6 +34,11 @@ class FinishedDetails extends Controller
             $page->render();
             die();
         }
+
+        if(!empty($_POST)){
+            $this->usePost();
+        }
+
         //Get model
         $modelManager=new ModelManager($this->dbConnection);
         $model=$modelManager->getOneFullById($id,$this->userId);
@@ -46,10 +52,12 @@ class FinishedDetails extends Controller
         if($dirPictures){
             //Get pictures if there are
             $baseDir=dirname(dirname(dirname(__DIR__))) . '/public/';
-            $scan=scandir($baseDir . $dirPictures);
-            foreach($scan as $file){
-                if($file!=='.' && $file!=='..'){
-                    $this->files[]=$dirPictures .'/' . $file;
+            if(is_dir($baseDir . $dirPictures)){
+                $scan=scandir($baseDir . $dirPictures);
+                foreach($scan as $file){
+                    if($file!=='.' && $file!=='..'){
+                        $this->files[]=$dirPictures .'/' . $file;
+                    }
                 }
             }
         }
@@ -86,6 +94,62 @@ class FinishedDetails extends Controller
                 $message->avatar=$baseUrl;
             }
             $this->messages[]=$message;
+        }
+    }
+
+    private function usePost()
+    {
+        var_dump($_POST);
+        if(!isset($_POST['action'])){
+            return;
+        }
+        switch($_POST['action']){
+            case 'deletePicture' : 
+                    if(!isset($_POST['file'])) return;
+                    $this->deletePicture($_POST['file']);
+                break;
+            case '' :
+                break;
+            default: return;
+        }
+    }
+
+    private function deletePicture(string $filename)
+    {
+        if(!isset($_POST['id']) || intval($_POST['id'])===0) return;
+        $idModel=intval($_POST['id']);
+        $needle="assets/uploads/users/{$this->userId}";
+        if(!str_contains($filename,$needle)){
+            return;
+        }
+        $baseDir=dirname(dirname(dirname(__DIR__))) . '/public/';
+        $fileToDelete=$baseDir . $filename;
+        if(!file_exists($fileToDelete) || !is_file($fileToDelete)){
+            return;
+        }
+        if(unlink($fileToDelete)){
+            /*Check if there are more files.
+            If not, delete from DB pictures */
+            $path=explode('/',$filename);
+            array_pop($path);
+            $fileDirectory=implode('/',$path);
+            if(is_dir($baseDir . $fileDirectory)){
+                $scan=scandir($baseDir . $fileDirectory);
+                $countFiles=0;
+                foreach($scan as $file){
+                    if($file!=='.' && $file!=='..'){
+                        $countFiles++;
+                    }
+                }
+                if($countFiles===0){
+                    $dirRemoved=rmdir($baseDir . $fileDirectory);
+                    if($dirRemoved){
+                        $modelManager=new ModelManager($this->dbConnection);
+                        //update field in DB, with picture=null;
+                        $modelManager->updatelinkModelUser($idModel,$this->userId,null);
+                    }
+                }
+            }
         }
     }
 }

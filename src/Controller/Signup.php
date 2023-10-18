5fd70d5deaa4170374b67b53e062ab7030de74af
Guillaume Crégut <gcregut@free.fr>
2023-10-18 23:05:47 +0200

    [Refactore]: Signup Controller: move sendMail user, only if account is created

--- src/Controller/Signup.php
+++ src/Controller/Signup.php
@@ -46,18 +46,18 @@ class  Signup extends Controller
                     $isOK=$auth->signUp($login,$email,$firstname,$lastname,$pass);
                     if($isOK){
                         $this->smarty->assign('success',true);
+                        $emitter=Emitter::getInstance();
+                        $emitter->emit(Emitter::USER_SUBSCRIBED,"Un nouvel utilisateur s'est inscrit");
+                        $mailer=new Mailer(); 
+                        $values=[
+                            'firstname'=>$firstname,
+                            'lastname'=>$lastname
+                        ];
+                        $mailer->sendHTMLMailToUser($email,'votre inscription à Model Kits Database',$values,'signup');
                     }
                     else{
                         $this->smarty->assign('error',"Votre compte n'a pas été enregistré. Veuillez contacter l'administrateur");
                     }
-                    $emitter=Emitter::getInstance();
-                    $emitter->emit(Emitter::USER_SUBSCRIBED,"Un nouvel utilisateur s'est inscrit");
-                    $mailer=new Mailer(); 
-                    $values=[
-                        'firstname'=>$firstname,
-                        'lastname'=>$lastname
-                    ];
-                    $mailer->sendHTMLMailToUser($email,'votre inscription à Model Kits Database',$values,'signup');
                 }
                 catch(Exception $e){
                     $errPage=new Error('500',$e->getMessage());

0b904b567c334d951a277e935141310e1f6b7c27
Guillaume Crégut <gcregut@free.fr>
2023-10-24 18:46:05 +0200

    [Feat]: friendbuild details controller: remove debug, add post message

--- src/Controller/Profil/FriendBuild.php
+++ src/Controller/Profil/FriendBuild.php
@@ -9,9 +9,10 @@ use Editiel98\Router\Controller;
 
 class FriendBuild extends Controller
 {
+    private SocialManager $socialManager;
     public function render()
     {
-        var_dump($_POST);
+        $this->socialManager = new SocialManager($this->dbConnection);
         if (!$this->isConnected) {
             //Render antoher page and die
             $this->smarty->assign('profil', 'profil');
@@ -25,6 +26,9 @@ class FriendBuild extends Controller
             case 'go':
                 $this->displayPage();
                 break;
+            case 'add':
+                $this->storeMessage();
+                break;
             default:
                 $this->displayError();
         }
@@ -37,44 +41,39 @@ class FriendBuild extends Controller
         }
         $idModel = intval($_POST['id']);
         $idFriend = intval($_POST['friend']);
-        $socialManager = new SocialManager($this->dbConnection);
-        $models=$socialManager->getFriendModelDetails($idFriend,$idModel);
-        $model=$models[0];
-        var_dump($model);
+        $models = $this->socialManager->getFriendModelDetails($idFriend, $idModel);
+        if (empty($models)) {
+            $this->displayError();
+        }
+        $model = $models[0];
         //get pictures
-        $dirPictures=$model->pictures;
-        $files=[];
-        if($dirPictures){
+        $dirPictures = $model->pictures;
+        $files = [];
+        if ($dirPictures) {
             //Get pictures if there are
-            $baseDir=dirname(dirname(dirname(__DIR__))) . '/public/';
-            if(is_dir($baseDir . $dirPictures)){
-                $scan=scandir($baseDir . $dirPictures);
-                foreach($scan as $file){
-                    if($file!=='.' && $file!=='..'){
-                        $files[]=$dirPictures .'/' . $file;
+            $baseDir = dirname(dirname(dirname(__DIR__))) . '/public/';
+            if (is_dir($baseDir . $dirPictures)) {
+                $scan = scandir($baseDir . $dirPictures);
+                foreach ($scan as $file) {
+                    if ($file !== '.' && $file !== '..') {
+                        $files[] = $dirPictures . '/' . $file;
                     }
                 }
             }
         }
-        var_dump($files);
         //get messages
-        $messages=$socialManager->getModelMessages($idModel);
-        var_dump($messages);
-
-        //for debug
-        $idFriend=14;
-
-        if(!empty($files)){
-            $this->smarty->assign('pictures',$files);
+        $messages = $this->socialManager->getModelMessages($idModel);
+        if (!empty($files)) {
+            $this->smarty->assign('pictures', $files);
         }
-        if($model->allow===1){
-            $this->smarty->assign('allow',true);
+        if ($model->allow === 1) {
+            $this->smarty->assign('allow', true);
         }
-        if(!empty($messages)){
-            $this->smarty->assign('messages',$messages);
+        if (!empty($messages)) {
+            $this->smarty->assign('messages', $messages);
         }
-        $this->smarty->assign('model',$model);
-        $this->smarty->assign('friend',$idFriend);
+        $this->smarty->assign('model', $model);
+        $this->smarty->assign('friend', $idFriend);
         $this->smarty->assign('profil', true);
         $this->smarty->assign('social_menu', true);
         $this->smarty->display('profil/friendbuild.tpl');
@@ -83,6 +82,12 @@ class FriendBuild extends Controller
     private function storeMessage()
     {
         //Do some stuff
+        if (!isset($_POST['id']) || intval($_POST['id']) === 0 || !isset($_POST['message']) || $_POST['message'] === '' || !isset($_POST['friend']) || intval($_POST['friend']) === 0) {
+            $this->displayError();
+        }
+        $model = intval($_POST['id']);
+        $message = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
+        $this->socialManager->postModelMessage($this->userId, $message, $model);
         $this->displayPage();
     }
 

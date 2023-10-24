fe83c54ffdc62364c283addfdf0e0e1885599cf1
Guillaume Crégut <gcregut@free.fr>
2023-10-24 22:58:39 +0200

    [Feat]: userController: add remove user function

--- src/Controller/Admin/Users.php
+++ src/Controller/Admin/Users.php
@@ -1,26 +1,50 @@
 <?php
+
 namespace App\Controller\Admin;
 
 use Editiel98\App;
+use Editiel98\Flash;
 use Editiel98\Manager\UserManager;
 use Editiel98\Router\Controller;
 
 class Users extends Controller
 {
+    private UserManager $userManager;
+
     public function render()
     {
-        if(App::ADMIN===$this->userRank){
-        
-            $userManager=new UserManager($this->dbConnection);
-            $users=$userManager->getAll();
-            //var_dump($users);
-            $this->smarty->assign('defaultUser',App::DEFAULT_ADMIN);
-            $this->smarty->assign('users',$users);
+        if (App::ADMIN === $this->userRank) {
+            $this->userManager = new UserManager($this->dbConnection);
+            if (!empty($_POST)) {
+                $this->usePost();
+            }
+            $this->hasFlash = $this->flash->hasFlash();
+            if ($this->hasFlash) {
+                $flashes = $this->flash->getFlash();
+                $this->smarty->assign('flash', $flashes);
+            }
+            $users = $this->userManager->getAll();
+            $this->smarty->assign('defaultUser', App::DEFAULT_ADMIN);
+            $this->smarty->assign('users', $users);
             $this->smarty->display('admin/users.tpl');
-        }else{
-            $this->smarty->assign('accueil','accueil');
+        } else {
+            $this->smarty->assign('accueil', 'accueil');
             $this->smarty->display('index.tpl');
         }
+    }
 
+    private function usePost()
+    {
+        if (!isset($_POST['action']) || $_POST['action'] !== 'delete') {
+            return;
+        }
+        if (!isset($_POST['id']) || intval($_POST['id']) === 0) {
+            return;
+        }
+        $id = intval($_POST['id']);
+        $result = $this->userManager->removeUser($id);
+        if (!$result) {
+            $this->flash->setFlash('Une erreur est survenue', Flash::FLASH_ERROR);
+        }
     }
 }

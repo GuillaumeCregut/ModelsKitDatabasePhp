4d97a96eb31f4bded3b7837b717fcb7d515ab556
Guillaume Crégut <gcregut@free.fr>
2023-12-02 17:55:14 +0100

    [Fix]: Index: change require string

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ use Editiel98\Autoloader;
 use Editiel98\App;
 
 session_start();
-require '../src/classes/Autoloader.php';
+require __DIR__ . '/../src/classes/Autoloader.php';
 Autoloader::register();
 $app=new App();
 $app->run();  

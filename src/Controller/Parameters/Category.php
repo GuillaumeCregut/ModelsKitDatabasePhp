d2339cc9dd1ae521d721fa351a0b4be4ba2279b4
Guillaume Crégut <gcregut@free.fr>
2023-12-23 19:07:10 +0100

    [Fix]: Rigths on moderate to update

--- src/Controller/Parameters/Category.php
+++ src/Controller/Parameters/Category.php
@@ -101,7 +101,7 @@ class Category extends Controller
 
     private function update(int $id, string $name): bool 
     {
-        if(App::ADMIN!==$this->userRank || App::MODERATE!==$this->userRank){
+        if(!(App::ADMIN===$this->userRank || App::MODERATE==$this->userRank)){
             return false;
         }
         $category=new EntityCategory();

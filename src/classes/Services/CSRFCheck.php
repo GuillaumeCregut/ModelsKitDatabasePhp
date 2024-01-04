b16cb5a659935d45c780673d4ffaef458b0d447c
Guillaume Crégut <gcregut@free.fr>
2024-01-04 14:25:18 +0100

    [Feat]: CSRFCheck: create service

---
+++ src/classes/Services/CSRFCheck.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Editiel98\Services;
+
+use Editiel98\Session;
+use Exception;
+
+class CSRFCheck
+{
+    private Session $session;
+
+    public function __construct(Session $session){
+        $this->session=$session;
+    }
+
+    /**
+     * createToken : create a CSRF token for forms
+     *
+     * @return string :hash to display in form
+     */
+    public function createToken(): string
+    {
+        //Get Secret
+        try {
+            $secret = $this->getSecret();
+        } catch (Exception $e) {
+            var_dump($e);
+            return '';
+        }
+        //generate token
+        $varPart = bin2hex(random_bytes(24));
+        $this->session->setKey('token_part', $varPart);
+        $token = $this->hashToken($varPart, $secret);
+        //Return tocken
+        return $token;
+    }
+
+
+    /**
+     * checkToken
+     *
+     * @param string $token : the token to check
+     * @return boolean : true if token OK, false else
+     */
+    public function checkToken(string $token): bool
+    {
+        //Get Secret
+        try {
+            $secret = $this->getSecret();
+        } catch (Exception $e) {
+            var_dump($e);
+            return '';
+        }
+        $varPart = $this->session->getKey('token_part');
+        //Decode Token
+        return hash_equals($this->hashToken($varPart, $secret), $token);
+    }
+
+    private function getSecret(): string
+    {
+        try {
+            $config = simplexml_load_file(dirname(dirname(__DIR__)) . '/config.xml');
+            if ($config === false) {
+                throw new Exception('Config not readable');
+            }
+            return $config->security->token;
+        } catch (Exception $e) {
+            throw new Exception('Config not readable');
+        }
+    }
+
+    private function hashToken(string $varPart, string $secret): string
+    {
+        return hash_hmac('sha256', $varPart, $secret);
+    }
+}
